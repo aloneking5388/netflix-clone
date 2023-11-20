caac16d372cbc118dd6b558d861d4bb4f87aebb8
alineking5388 <alineking5388@gmail.com>
2023-11-20 23:43:46 +0530

    title change

--- app/components/UserNav.tsx
+++ app/components/UserNav.tsx
@@ -19,7 +19,7 @@ export default function UserNav() {
         <Button variant="ghost" className="relative h-10 w-10 rounded-sm">
           <Avatar className="h-10 w-10 rounded-sm">
             <AvatarImage src="https://nkljkwikhlggqlnjctge.supabase.co/storage/v1/object/public/user%20image/avatar.png" />
-            <AvatarFallback className="rounded-sm">Jan</AvatarFallback>
+            <AvatarFallback className="rounded-sm">Alone</AvatarFallback>
           </Avatar>
         </Button>
       </DropdownMenuTrigger>
